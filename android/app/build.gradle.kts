7291b7ac1c8c698e3e036e449a818f8c6de4f77c
minhsond1235 <minhsond1235@gmail.com>
2026-05-17 20:23:47 +0700

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.ducvuong_2224802010753_lab2_part1"
+    namespace = "com.example.daominhson_2224802010371_lab2_part1"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.ducvuong_2224802010753_lab2_part1"
+        applicationId = "com.example.daominhson_2224802010371_lab2_part1"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
